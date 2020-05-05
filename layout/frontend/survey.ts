1ac66f61b0e2a32c6b98d6aa9377703f7fa78448
M. Vondano <m-vo@users.noreply.github.com>
2020-05-05 19:19:23 +0200

    improve form GUI

--- layout/frontend/survey.ts
+++ layout/frontend/survey.ts
@@ -1,12 +1,17 @@
 function clickingOnUserValueSelectsOption(container: HTMLElement) {
-    container.querySelectorAll<HTMLInputElement>('input.user_option').forEach(userInput => {
-        userInput.addEventListener('click', () => {
-            const input = userInput.parentElement.querySelector<HTMLInputElement>('input[type="checkbox"]');
-            if(null === input) {
+    container.querySelectorAll<HTMLInputElement>('.option .user_option').forEach(userOption => {
+        userOption.addEventListener('click', () => {
+            const checkbox = userOption.parentElement.querySelector<HTMLInputElement>('input[type="checkbox"], input[type="radio"]');
+            if (null === checkbox) {
                 return;
             }
+            checkbox.checked = true;
 
-            input.checked = true;
+            const input = userOption.querySelector<HTMLInputElement>('input[type="text"]');
+            if (null === input) {
+                return;
+            }
+            input.focus();
         });
     })
 }
@@ -25,12 +30,64 @@ function clickingOnTableHeaderSelectsAllOfColumn(container: HTMLElement) {
             const index = header.getAttribute('data-option');
             header
                 .closest('table')
-                .querySelectorAll<HTMLInputElement>('tbody td[data-option="'+ index +'"] input')
+                .querySelectorAll<HTMLInputElement>('tbody td[data-option="' + index + '"] input')
                 .forEach(input => {
                     input.checked = true;
                 })
         });
     })
+}
+
+function oversizedTableDisplayScrollNote(container: HTMLElement) {
+    const table = container.querySelector('table');
+    const scrollNote = container.querySelector('.note_scroll');
+
+    if (null === table || null === scrollNote) {
+        return;
+    }
+
+    const checkSize = () => {
+        scrollNote.classList.toggle('hidden', table.clientWidth <= table.parentElement.clientWidth)
+    };
+
+    checkSize();
+    (new ResizeObserver(checkSize)).observe(table.parentElement);
+}
+
+function resettingCurrentStepClearsForm(container: HTMLElement) {
+    container.querySelector<HTMLInputElement>('input[type="reset"').addEventListener('click', (resetElement) => {
+        container.querySelectorAll('input, textarea, select').forEach((el) => {
+            if (el === resetElement.target) {
+                return;
+            }
+
+
+            if (el instanceof HTMLInputElement) {
+                if (el.checked) {
+                    el.removeAttribute('checked');
+
+                    return;
+                }
+
+                if (el.type === 'text') {
+                    el.removeAttribute('value');
+                }
+
+                return;
+            }
+
+            if (el instanceof HTMLTextAreaElement) {
+                el.innerText = "";
+
+                return;
+            }
+
+            if (el instanceof HTMLSelectElement) {
+                el.selectedIndex = -1;
+                el.value = "";
+            }
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -43,4 +100,6 @@ document.addEventListener('DOMContentLoaded', () => {
     clickingOnUserValueSelectsOption(survey);
     clickingOnTableCellSelectsOption(survey);
     clickingOnTableHeaderSelectsAllOfColumn(survey);
+    oversizedTableDisplayScrollNote(survey);
+    resettingCurrentStepClearsForm(survey);
 })
