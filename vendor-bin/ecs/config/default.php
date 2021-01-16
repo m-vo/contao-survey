f78054f201d61a6d11500c85f6ea3de954c684bd
M. Vondano <m-vo@users.noreply.github.com>
2021-01-16 18:52:57 +0100

    rework tooling

---
+++ vendor-bin/ecs/config/default.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Contao\EasyCodingStandard\Sniffs\ContaoFrameworkClassAliasSniff;
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../vendor/contao/easy-coding-standard/config/default.php');
+
+    $services = $containerConfigurator->services();
+
+    $services
+        ->set(HeaderCommentFixer::class)
+        ->call('configure', [[
+            'header' => "@author  Moritz Vondano\n@license MIT",
+        ]]);
+
+    $services
+        ->set(PhpUnitTestCaseStaticMethodCallsFixer::class)
+        ->call('configure', [[
+            'call_type' => 'self',
+        ]])
+    ;
+
+    $services
+        ->set(ContaoFrameworkClassAliasSniff::class, \stdClass::class);
+};
