01df373a870a7fc08a967497c9b845874d2b3f12
David Molineus <david.molineus@netzmacht.de>
2023-05-12 09:51:31 +0200

    Prevent undefined array index access for PHP 8 compatibility

--- src/Resources/contao/dca/tl_survey.php
+++ src/Resources/contao/dca/tl_survey.php
@@ -36,7 +36,7 @@ $GLOBALS['TL_DCA']['tl_survey'] =
                 'delete' => [
                     'href' => 'act=delete',
                     'icon' => 'delete.svg',
-                    'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                    'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '').'\'))return false;Backend.getScrollOffset()"',
                 ],
             ],
         ],
